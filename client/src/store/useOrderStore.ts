e2044522303c348d5d943bac65b2b6e4f4e02153
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2024-12-17 23:32:23 +0530

    url changes

--- client/src/store/useOrderStore.ts
+++ client/src/store/useOrderStore.ts
@@ -3,8 +3,7 @@ import axios from "axios";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-// const API_END_POINT: string = "https://food-app-yt.onrender.com/api/v1/order";
-const API_END_POINT = "http://localhost:8000/api/v1/order"
+const API_END_POINT: string = "https://restaurant-website-MERN-live.onrender.com/api/v1/order";
 
 axios.defaults.withCredentials = true;
 
